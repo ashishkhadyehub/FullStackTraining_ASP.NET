c7b2ffa360d13a5118c50bdea6f41633fc037556
ashishkhadye33 <ashishkhadye33@gmail.com>
2023-07-29 13:36:32 +0530

    updated with gv example

---
+++ Demo14April/gvexample.aspx
@@ -0,0 +1,43 @@
+﻿<%@ Page Title="" Language="C#" MasterPageFile="~/User.Master" AutoEventWireup="true" CodeBehind="gvexample.aspx.cs" Inherits="Demo14April.gvexample" %>
+
+<asp:Content ID="Content1" ContentPlaceHolderID="head" runat="server">
+</asp:Content>
+<asp:Content ID="Content2" ContentPlaceHolderID="ContentPlaceHolder1" runat="server">
+    <div class="container" style="margin-top: 50px">
+        <div class="row">
+            <h3>Data Control- ASP.NET Gridview</h3>
+        </div>
+        <div class="row">
+            <asp:GridView ID="gvdata" OnRowEditing="gvdata_RowEditing" OnRowDeleting="gvdata_RowDeleting" OnRowCancelingEdit="gvdata_RowCancelingEdit" OnRowUpdating="gvdata_RowUpdating" AutoGenerateColumns="false" DataKeyNames="Srno" CssClass="table table-striped" runat="server" EmptyDataText="No data found">
+                <Columns>
+                    <asp:TemplateField HeaderText="Name" ItemStyle-Width="150">
+                        <ItemTemplate>
+                            <asp:Label ID="lblName" runat="server" Text='<%# Eval("Name") %>'></asp:Label>
+                        </ItemTemplate>
+                        <EditItemTemplate>
+                            <asp:TextBox ID="txtname" runat="server" Text='<%# Eval("Name") %>'></asp:TextBox>
+                        </EditItemTemplate>
+                    </asp:TemplateField>
+                    <asp:TemplateField HeaderText="Email" ItemStyle-Width="150">
+                        <ItemTemplate>
+                            <asp:Label ID="lblEmail" runat="server" Text='<%# Eval("Email") %>'></asp:Label>
+                        </ItemTemplate>
+                        <EditItemTemplate>
+                            <asp:TextBox ID="txtemail" runat="server" Text='<%# Eval("Email") %>'></asp:TextBox>
+                        </EditItemTemplate>
+                    </asp:TemplateField>
+                    <asp:TemplateField HeaderText="Contact" ItemStyle-Width="150">
+                        <ItemTemplate>
+                            <asp:Label ID="lblContact" runat="server" Text='<%# Eval("Contact") %>'></asp:Label>
+                        </ItemTemplate>
+                        <EditItemTemplate>
+                            <asp:TextBox ID="txtcontact" runat="server" Text='<%# Eval("Contact") %>'></asp:TextBox>
+                        </EditItemTemplate>
+                    </asp:TemplateField>
+                     <asp:CommandField ControlStyle-CssClass="btn btn-primary" ButtonType="Link" ShowEditButton="true" ShowDeleteButton="true"
+            ItemStyle-Width="300" />
+                </Columns>
+            </asp:GridView>
+        </div>
+    </div>
+</asp:Content>
---
+++ Demo14April/gvexample.aspx.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace Demo14April
+{
+    public partial class gvexample : System.Web.UI.Page
+    {
+        SqlConnection con = new SqlConnection(ConfigurationManager.ConnectionStrings["connstr"].ConnectionString);
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if(!IsPostBack)
+            {
+                gvdata.DataSource = getdata();
+                gvdata.DataBind();
+            }
+        }
+
+        protected DataSet getdata()
+        {
+            con.Close();
+            SqlCommand cmd = new SqlCommand("select * from EmpRegister order by Srno ASC", con);
+            con.Open();
+            DataSet ds = new DataSet();
+            SqlDataAdapter sda = new SqlDataAdapter(cmd);
+            sda.Fill(ds);
+            con.Close();
+            return ds;
+        }
+
+        protected void gvdata_RowEditing(object sender, GridViewEditEventArgs e)
+        {
+            gvdata.EditIndex = e.NewEditIndex;
+            gvdata.DataSource = getdata();
+            gvdata.DataBind();
+        }
+
+        protected void gvdata_RowCancelingEdit(object sender, GridViewCancelEditEventArgs e)
+        {
+            gvdata.EditIndex = -1;
+            gvdata.DataSource = getdata();
+            gvdata.DataBind();
+        }
+
+        protected void gvdata_RowUpdating(object sender, GridViewUpdateEventArgs e)
+        {
+            try
+            {
+                con.Close();
+                GridViewRow row = gvdata.Rows[e.RowIndex];
+                string name = (row.FindControl("txtname") as TextBox).Text;
+                string email = (row.FindControl("txtemail") as TextBox).Text;
+                string contact = (row.FindControl("txtcontact") as TextBox).Text;
+
+
+                SqlCommand cmd = new SqlCommand("Update EmpRegister set Name='" + name + "',Email='" + email + "',Contact='"+contact+"' where Srno='" + gvdata.DataKeys[e.RowIndex].Value + "'", con);
+                con.Open();
+                cmd.ExecuteNonQuery();
+                con.Close();
+                gvdata.EditIndex = -1;
+                gvdata.DataSource = getdata();
+                gvdata.DataBind();
+                
+            }
+            catch (Exception es)
+            {
+
+            }
+
+        }
+
+        protected void gvdata_RowDeleting(object sender, GridViewDeleteEventArgs e)
+        {
+            con.Close();
+            GridViewRow row = gvdata.Rows[e.RowIndex];
+
+            SqlCommand cmd = new SqlCommand("Delete from EmpRegister where Srno='" + gvdata.DataKeys[e.RowIndex].Value + "'", con);
+            con.Open();
+            cmd.ExecuteNonQuery();
+            con.Close();
+            gvdata.EditIndex = -1;
+            gvdata.DataSource = getdata();
+            gvdata.DataBind();
+        }
+    }
+}
